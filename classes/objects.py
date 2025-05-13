ce4a3adf03b93169870a3e3a0d3264abecb05bc1
u7381813 <u7381813@anu.edu.au>
2025-05-13 20:20:39 +1000

    Added State Design Pattern - states.py

--- classes/objects.py
+++ classes/objects.py
@@ -1,4 +1,6 @@
 class Patient:
-  def __init__(self, name, age):
+  def __init__(self, name, age, CT, fluroscopy):
     self.name = name
     self.age = age
+    self.CT = CT
+    self.fluroscopy = fluroscopy
