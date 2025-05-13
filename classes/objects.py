018ef0fca4f76ed8c176664fe9e226e4b4a70612
u7381813 <u7381813@anu.edu.au>
2025-05-13 21:42:02 +1000

    Added Singleton Design Pattern - singleton.py

--- classes/objects.py
+++ classes/objects.py
@@ -1,6 +1,16 @@
+# Need revision 
 class Patient:
   def __init__(self, name, age, CT, fluroscopy):
     self.name = name
     self.age = age
     self.CT = CT
     self.fluroscopy = fluroscopy
+
+  
+  @property
+  def name(self):
+    return self._name
+
+  @name.setter
+  def name(self, value):
+    self._name = value.upper()
