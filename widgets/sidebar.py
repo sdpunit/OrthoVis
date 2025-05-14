4cb899ca71cb5cab3ab7576657bef698db639bd7
Murph Shaw <u7311689@anu.edu.au>
2025-05-14 17:58:15 +1000

    Added sidebar file for continuing next week

---
+++ widgets/sidebar.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton
+from PySide6.QtGui import QIcon
+from PySide6.QtCore import QSize
+
+class Sidebar(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # Space for the nav bar ui file here
