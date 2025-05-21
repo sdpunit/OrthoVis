5e9546776913b69c619b75fe69d47e363302d198
Murph Shaw <u7311689@anu.edu.au>
2025-05-21 16:46:34 +1000

    Updated segmentation pages

--- frontend_pages/segmentation/ui_segmentation_window.py
+++ frontend_pages/segmentation/ui_segmentation_window.py
@@ -15,7 +15,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHBoxLayout, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QPushButton,
+    QSizePolicy, QTextEdit, QWidget)
 
 from widgets.sidebar.sidebar import Sidebar
 
@@ -23,7 +24,7 @@ class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(640, 600)
+        Form.resize(825, 600)
         self.horizontalLayout = QHBoxLayout(Form)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.widget = Sidebar(Form)
@@ -33,6 +34,21 @@ class Ui_Form(object):
 
         self.widget_2 = QWidget(Form)
         self.widget_2.setObjectName(u"widget_2")
+        self.segment_btn = QPushButton(self.widget_2)
+        self.segment_btn.setObjectName(u"segment_btn")
+        self.segment_btn.setGeometry(QRect(370, 530, 151, 41))
+        self.frame = QFrame(self.widget_2)
+        self.frame.setObjectName(u"frame")
+        self.frame.setGeometry(QRect(30, 40, 451, 461))
+        self.frame.setStyleSheet(u"background-color: grey;")
+        self.frame.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Shadow.Raised)
+        self.textEdit = QTextEdit(self.frame)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setGeometry(QRect(160, 210, 161, 31))
+        self.textEdit.setStyleSheet(u"text-align: center;\n"
+"color: white;\n"
+"border: none;")
 
         self.horizontalLayout.addWidget(self.widget_2)
 
@@ -46,5 +62,14 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.segment_btn.setText(QCoreApplication.translate("Form", u"Segment CT", None))
+        self.textEdit.setHtml(QCoreApplication.translate("Form", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'.AppleSystemUIFont'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:700;\">VTK Display Window</span></p></body></html>", None))
     # retranslateUi
 
