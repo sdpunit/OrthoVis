2ba1f4de5136f21e4d158184fcb13e03dd2df91d
Hanna Truong <u7735208@anu.edu.au>
2025-05-21 14:55:19 +1000

    Initialized layout for new project window and added sidebar and titlebar

--- frontend_pages/new_project/ui_new_project_window.py
+++ frontend_pages/new_project/ui_new_project_window.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'new_project_window.ui'
+## Form generated from reading UI file 'new_project_windowWzVYGL.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.0
 ##
@@ -15,29 +15,55 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHBoxLayout, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QSizePolicy,
+    QVBoxLayout, QWidget)
 
 from widgets.sidebar.sidebar import Sidebar
+from widgets.titlebar.titlebar import Titlebar
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(640, 600)
+        Form.resize(699, 607)
         self.horizontalLayout = QHBoxLayout(Form)
+        self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.widget = Sidebar(Form)
-        self.widget.setObjectName(u"widget")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.sidebar = Sidebar(Form)
+        self.sidebar.setObjectName(u"sidebar")
 
-        self.horizontalLayout.addWidget(self.widget)
+        self.horizontalLayout.addWidget(self.sidebar)
 
-        self.widget_2 = QWidget(Form)
-        self.widget_2.setObjectName(u"widget_2")
+        self.mainpanel = QWidget(Form)
+        self.mainpanel.setObjectName(u"mainpanel")
+        self.verticalLayout = QVBoxLayout(self.mainpanel)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.titlebar = Titlebar(self.mainpanel)
+        self.titlebar.setObjectName(u"titlebar")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.titlebar.sizePolicy().hasHeightForWidth())
+        self.titlebar.setSizePolicy(sizePolicy)
+        self.titlebar.setMinimumSize(QSize(0, 70))
 
-        self.horizontalLayout.addWidget(self.widget_2)
+        self.verticalLayout.addWidget(self.titlebar)
+
+        self.frame = QFrame(self.mainpanel)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Shadow.Raised)
+
+        self.verticalLayout.addWidget(self.frame)
+
+
+        self.horizontalLayout.addWidget(self.mainpanel)
 
         self.horizontalLayout.setStretch(0, 1)
-        self.horizontalLayout.setStretch(1, 2)
+        self.horizontalLayout.setStretch(1, 3)
 
         self.retranslateUi(Form)
 
