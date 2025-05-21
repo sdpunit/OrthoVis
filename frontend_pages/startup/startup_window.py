0cb01869ccef48e985ce7ff2d9f793d7e90ab264
Hanna Truong <u7735208@anu.edu.au>
2025-05-21 12:18:14 +1000

    Tested the sidebar widgets and reorganize the files

--- frontend_pages/startup/startup_window.py
+++ frontend_pages/startup/startup_window.py
@@ -1,6 +1,6 @@
 # Startup window on opening of the application
 from PySide6.QtWidgets import QWidget
-from frontend_pages.ui_startup_window import Ui_HomePage
+from frontend_pages.startup.ui_startup_window import Ui_HomePage
 
 class HomePage(QWidget):
     def __init__(self):
---
+++ widgets/ui_sidebar.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'sidebaruICtna.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QSizePolicy, QWidget)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(640, 480)
+        self.label = QLabel(Form)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(30, 10, 81, 51))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.label.setText(QCoreApplication.translate("Form", u"test sidebar", None))
+    # retranslateUi
+
